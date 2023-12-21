9c35fcf75c32e3dc5ee3929d7fa5497567189a9a
Hasnain Ali <61244281+hasnainali9@users.noreply.github.com>
2023-12-21 11:29:15 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,42 @@
+function skillsMember(){
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkills2 = document.getElementById("memberSkills2");
+    var memberSkills3 = document.getElementById("memberSkills3");
+    var memberSkills4 = document.getElementById("memberSkills4");
+    var memberSkills5 = document.getElementById("memberSkills5");
+    var memberSkills6 = document.getElementById("memberSkills6");
+    var memberSkills7 = document.getElementById("memberSkills7");
+    var memberSkills8 = document.getElementById("memberSkills8");
+    var memberSkills9 = document.getElementById("memberSkills9");
+    var memberSkills10 = document.getElementById("memberSkills10");
+    var memberSkills11 = document.getElementById("memberSkills11");
+    var memberSkills12 = document.getElementById("memberSkills12");
+    var memberSkills13 = document.getElementById("memberSkills13");
+    var memberSkills14 = document.getElementById("memberSkills14");
+    var memberSkills15 = document.getElementById("memberSkills15");
+    var memberSkills16 = document.getElementById("memberSkills16");
+    var memberSkills17 = document.getElementById("memberSkills17");
+    var memberSkills18 = document.getElementById("memberSkills18");
+    var memberSkills19 = document.getElementById("memberSkills19");
+    var memberSkills20 = document.getElementById("memberSkills20");
+    var memberSkills21 = document.getElementById("memberSkills21");
+    var memberSkills22 = document.getElementById("memberSkills22");
+    var memberSkills23 = document.getElementById("memberSkills23");
+    var memberSkills24 = document.getElementById("memberSkills24");
+    var memberSkills25 = document.getElementById("memberSkills25");
+    var memberSkills26 = document.getElementById("memberSkills26");
+    var memberSkills27 = document.getElementById("memberSkills27");
+    var memberSkills28 = document.getElementById("memberSkills28");
+    var memberSkills29 = document.getElementById("memberSkills29");
+    var memberSkills30 = document.getElementById("memberSkills30");
+    var memberSkills31 = document.getElementById("memberSkills31");
+    var memberSkills32 = document.getElementById("memberSkills32");
+    var memberSkills33 = document.getElementById("memberSkills33");
+    var memberSkills34 = document.getElementById("memberSkills34");
+    var memberSkills35 = document.getElementById("memberSkills35");
+    var memberSkills36 = document.getElementById("memberSkills36");
+    var memberSkills37 = document.getElementById("memberSkills37");
+    var    
+}
